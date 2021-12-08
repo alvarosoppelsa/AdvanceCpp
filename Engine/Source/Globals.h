3aa598137341ce378044179e2df6116d3198929a
Alvaro Soppelsa <alvaro.soppelsa1@hp.com>
2021-12-08 18:52:03 +0100

    Back to master repo

--- Engine/Source/Globals.h
+++ Engine/Source/Globals.h
@@ -19,7 +19,7 @@ enum update_status
 #define FULLSCREEN false
 #define VSYNC true
 #define TITLE "Game Engine"
-#define MY_REPO "https://github.com/alvarosoppelsa/AdvanceCpp/tree/develop/Engine"
+#define MY_REPO "https://github.com/alvarosoppelsa/AdvanceCpp/tree/master/Engine"
 #define LIBRARIES_USED "SDL 2.0.16\nGLEW 2.1.0\nDear ImGui 1.86\nDevIL 1.8.0"
 
 // Shaders ----------------
