325aa905dfc04774121104be696ca2291fd42f09
alvarosoppelsa <alvarosoppelsa@gmail.com>
2021-12-09 21:21:49 +0100

    Load multiple textures

--- Engine/Source/Mesh.cpp
+++ Engine/Source/Mesh.cpp
@@ -43,40 +43,48 @@ void Mesh::SetupMesh()
 
 void Mesh::Draw(const unsigned int programId, const float4x4& view, const float4x4& proj, const float4x4& model)
 {
-    //unsigned int diffuseNr = 1;
-    //unsigned int specularNr = 1;
-    //for (unsigned int i = 0; i < Textures.size(); i++)
-    //{
-    //    glActiveTexture(GL_TEXTURE0 + i); // activate proper texture unit before binding
-    //    // retrieve texture number (the N in diffuse_textureN)
-    //    std::string number;
-    //    std::string name = Textures[i].Type;
-    //    if (name == "texture_diffuse")
-    //        number = std::to_string(diffuseNr++);
-    //    else if (name == "texture_specular")
-    //        number = std::to_string(specularNr++);
-    //    // TODO: Shader class is as our Program Module. Set float send a value to a uniform
-    //    //shader.setFloat(("material." + name + number).c_str(), i);
-    //    glBindTexture(GL_TEXTURE_2D, Textures[i].Id);
-    //}
-    //glActiveTexture(GL_TEXTURE0);
-    //// draw mesh
-    //glBindVertexArray(VAO);
-    //glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
-    //glBindVertexArray(0);
     glUseProgram(programId);
     glUniformMatrix4fv(glGetUniformLocation(programId, "model"), 1, GL_TRUE, (const float*)&model);
     glUniformMatrix4fv(glGetUniformLocation(programId, "view"), 1, GL_TRUE, (const float*)&view);
     glUniformMatrix4fv(glGetUniformLocation(programId, "proj"), 1, GL_TRUE, (const float*)&proj);
-    glActiveTexture(GL_TEXTURE0);
+
+    unsigned int diffuseNr = 1;
+    unsigned int specularNr = 1;
     for (unsigned int i = 0; i < Textures.size(); i++)
     {
-    	// TODO: For more than one texture it's needed to change texture names. See example above
+        glActiveTexture(GL_TEXTURE0 + i);
+        // activate proper texture unit before binding
+        // retrieve texture number (the N in diffuse_textureN)
+        std::string number;
+        std::string name = Textures[i].Type;
+        if (name == "texture_diffuse")
+        {
+            number = std::to_string(diffuseNr++);
+        }
+        else if (name == "texture_specular")
+        {
+            number = std::to_string(specularNr++);
+        }
+        //shader.setFloat(("material." + name + number).c_str(), i);
+        glUniform1i(glGetUniformLocation(programId, (name + number).c_str()), i);
         glBindTexture(GL_TEXTURE_2D, Textures[i].Id);
     }
-
-    glUniform1i(glGetUniformLocation(programId, "diffuse"), 0);
+    glActiveTexture(GL_TEXTURE0);
+    // draw mesh
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, nullptr);
+    glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, 0);
     glBindVertexArray(0);
+
+    //glActiveTexture(GL_TEXTURE0);
+    //for (unsigned int i = 0; i < Textures.size(); i++)
+    //{
+    //	// TODO: For more than one texture it's needed to change texture names. See example above
+    //    glBindTexture(GL_TEXTURE_2D, Textures[i].Id);
+    //}
+
+    //glUniform1i(glGetUniformLocation(programId, "diffuse"), 0);
+    // draw mesh
+    //glBindVertexArray(VAO);
+    //glDrawElements(GL_TRIANGLES, Indices.size(), GL_UNSIGNED_INT, nullptr);
+    //glBindVertexArray(0);
 }
--- Engine/Source/Shaders/FragmentShaderWithUniforms.glsl
+++ Engine/Source/Shaders/FragmentShaderWithUniforms.glsl
@@ -1,12 +1,17 @@
 #version 330 core
 
 out vec4 color;
-
+out vec4 shade;
 in vec2 uv0;
 
-uniform sampler2D diffuse;
+uniform sampler2D texture_diffuse1;
+uniform sampler2D texture_diffuse2;
+uniform sampler2D texture_diffuse3;
+uniform sampler2D texture_specular1;
+uniform sampler2D texture_specular2;
 
 void main()
 {
-	color = texture2D(diffuse, uv0);
+	color = texture2D(texture_diffuse1, uv0);
+	shade = texture2D(texture_diffuse2, uv0);
 }
